637ce3d81b9982b7f25bb7898b11272fd0fa064b
lucas barros <lucasbarrosrock@hotmail.com>
2022-11-17 12:40:51 -0300

    Add project files.

---
+++ SeleniumSetup/EntryPoint.cs
@@ -0,0 +1,36 @@
+﻿// Selenium test with c#
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumSetup;
+
+IWebDriver driver = new ChromeDriver();
+
+driver.Navigate().GoToUrl("https://testing.todorvachev.com/name/"); // abrir navegador na página específica
+
+IWebElement element = null;
+string name = "myName";
+string id = "testImage";
+string className = "testClass";
+string cssPath = "#post-108 > div > figure > img";
+string xPath = "//*[@id=\"post-109\"]/div/figure/img";
+//achar pelo nome
+FindSelectors selectors = new FindSelectors();
+selectors.SelectorByName(element, driver, name);
+//achar pelo ID
+driver.Navigate().GoToUrl("https://testing.todorvachev.com/id/");
+selectors.SelectorByID(element, driver, id);
+//achar pelo selector className
+driver.Navigate().GoToUrl("https://testing.todorvachev.com/class-name/");
+selectors.SelectorByClassName(element, driver, className);
+//achar pelo selector css 
+driver.Navigate().GoToUrl("https://testing.todorvachev.com/css-path/");
+selectors.SelectorByCSSPath(element, driver, cssPath);
+//achar pelo selector xPath 
+driver.Navigate().GoToUrl("https://testing.todorvachev.com/xpath/");
+selectors.SelectorByXPath(element, driver, xPath);
+
+
+
+driver.Quit(); //fechar o site;
+
---
+++ SeleniumSetup/FindSelectors.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace SeleniumSetup
+{
+    //encontra os seletores com base em Nome,ID, Class Name, CSS e XPath
+    public class FindSelectors
+    {
+
+        public void SelectorByName(IWebElement element, IWebDriver driver, string name)
+        {
+            element = driver.FindElement(By.Name(name));
+            if (element.Displayed)
+            {
+                GreenMessage("Yes, I can see the element");
+                Console.WriteLine(element.Text);
+            }
+            else
+            {
+                RedMessage("I can´t see the element");
+            }
+        }
+        public void SelectorByID(IWebElement element, IWebDriver driver, string id)
+        {
+            element = driver.FindElement(By.Id(id));
+            if (element.Displayed)
+            {
+                GreenMessage("Yes, I can see the element");
+                Console.WriteLine(element.Text);
+            }
+            else
+            {
+                RedMessage("I can´t see the element");
+            }
+        }
+        public void SelectorByClassName(IWebElement element, IWebDriver driver, string className)
+        {
+            element = driver.FindElement(By.ClassName(className));
+            if (element.Displayed)
+            {
+                GreenMessage("Yes, I can see the element");
+                Console.WriteLine(element.Text);
+            }
+            else
+            {
+                RedMessage("I can´t see the element");
+            }
+        }
+        public void SelectorByCSSPath(IWebElement element, IWebDriver driver, string cssPath)
+        {
+            element = driver.FindElement(By.CssSelector(cssPath));
+            if (element.Displayed)
+            {
+                GreenMessage("Yes, I can see the element");
+                Console.WriteLine(element.Text);
+            }
+            else
+            {
+                RedMessage("I can´t see the element");
+            }
+        }
+        public void SelectorByXPath(IWebElement element, IWebDriver driver, string XPath)
+        {
+            element = driver.FindElement(By.XPath(XPath));
+            if (element.Displayed)
+            {
+                GreenMessage("Yes, I can see the element");
+                Console.WriteLine(element.Text);
+            }
+            else
+            {
+                RedMessage("I can´t see the element");
+            }
+        }
+        void RedMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        void GreenMessage(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+    }
+}
